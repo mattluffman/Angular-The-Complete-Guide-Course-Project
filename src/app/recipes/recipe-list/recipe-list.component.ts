98e58ffaa3f0aa1b0859dc8e48d693cb0a2d1a3e
Matt Luffman <matt.luffman@gmail.com>
2020-05-16 17:00:09 -0400

    section 3, lecture 52 recipe model, & recipe list basic markup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('The Best Pizza!', 'this is the best pizza you\'ve ever had, trust me!', 'https://upload.wikimedia.org/wikipedia/commons/6/64/NYPizzaPie.jpg')
+    ];
 
     constructor() {
     }
