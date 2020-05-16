9408b73782b6effa416074d159870e3b982f2506
Matt Luffman <matt.luffman@gmail.com>
2020-05-16 17:30:12 -0400

    the way he did it with another recipe added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [
-        new Recipe('The Best Pizza!', 'this is the best pizza you\'ve ever had, trust me!', 'https://upload.wikimedia.org/wikipedia/commons/6/64/NYPizzaPie.jpg')
+        new Recipe('Little Italy Pizza!', 'this is the best pizza you\'ve ever had, trust me! ', 'https://upload.wikimedia.org/wikipedia/commons/6/64/NYPizzaPie.jpg'),
+        new Recipe('Donkey On Crack', 'Chuck\'s Fish preimer roll. Cream cheese Spicy sauce Green onion Avocado Tuna Crab Deep fried and topped with Baked seafood Japanese mayo Eel sauce Sriracha Uptown sauce.', 'https://s3-media0.fl.yelpcdn.com/bphoto/HmnSB02FTqSP5mrU9jPyDw/o.jpg')
     ];
 
     constructor() {
@@ -16,5 +17,4 @@ export class RecipeListComponent implements OnInit {
 
     ngOnInit(): void {
     }
-
 }
