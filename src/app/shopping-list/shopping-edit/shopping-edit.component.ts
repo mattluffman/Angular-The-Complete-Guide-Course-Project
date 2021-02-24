d8dc9a3bc1c479b1f45d32cd4068d498e36e9f87
Matt Luffman <matt.luffman@gmail.com>
2021-02-23 20:04:23 -0500

    lecture 90 - my first attempt with field clearing. Not resetting html form at this point.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
     selector: 'app-shopping-edit',
@@ -6,6 +7,9 @@ import {Component, OnInit} from '@angular/core';
     styles: []
 })
 export class ShoppingEditComponent implements OnInit {
+    @ViewChild('nameInput') nameField: ElementRef<HTMLInputElement>;
+    @ViewChild('amountInput') amountField: ElementRef<HTMLInputElement>;
+    @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
     constructor() {
     }
@@ -13,4 +17,12 @@ export class ShoppingEditComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    addIngredient(): void {
+        const item = new Ingredient(this.nameField.nativeElement.value, this.amountField.nativeElement.valueAsNumber);
+        this.ingredientAdded.emit(item);
+        // clear fields
+        this.nameField.nativeElement.value = '';
+        this.amountField.nativeElement.value = '';
+        // would be good to reset the form as well
+    }
 }
