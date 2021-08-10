82197ec57067f42d0260e62f86b121250c3e807b
Matt Luffman <matt.luffman@gmail.com>
2021-08-10 17:30:49 -0400

    rewatching lecture 117. Marking recipe list private so it can't be accessed directly outside of this service.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,7 +7,7 @@ import {Recipe} from './recipe.model';
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
-    recipes: Recipe[] = [
+    private recipes: Recipe[] = [
         new Recipe('Little Italy Pizza!', 'this is the best pizza you\'ve ever had, trust me! ', 'https://www.orderbulldawgfood.com/food/v_2_i33383671s.jpg'),
         new Recipe('Donkey On Crack', 'Chuck\'s Fish premier roll. Cream cheese Spicy sauce Green onion Avocado Tuna Crab Deep fried and topped with Baked seafood Japanese mayo Eel sauce Sriracha Uptown sauce.', 'https://s3-media0.fl.yelpcdn.com/bphoto/HmnSB02FTqSP5mrU9jPyDw/o.jpg')
     ];
